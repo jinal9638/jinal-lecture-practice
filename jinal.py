e0bb5cab1eaec832318a2f8b7d30764813c608e1
jinal9638 <jinalkhernar1312@gmail.com>
2026-05-13 15:35:49 +0530

    Add files via upload

---
+++ jinal.py
@@ -0,0 +1,46 @@
+#basic of python
+#function built in functions
+# 1 system function which is built by system
+# 2 user defined function is a custom that you create yourself in program while solving any type of code that you don't whant to type over over again
+
+"""
+print() like print("jinal") output will be jinal
+input() this function take input from user
+like name=input("type your name:") 
+id()
+len() this function define the character length like 'apple' have 5 character
+type() you can get the type likes string,float,int,boolean
+etc
+"""
+print("Hello, everyone! I am learning python...")
+
+#add two numbers
+a=1
+b=2
+
+print("Add:",a+b)
+
+name=input("type your name:")
+print("how are you",name,"?")
+print(type(a))
+
+#calculation
+x=int(input("enter a no.:"))
+y=int(input("enter a no.:"))
+
+
+print("Add:",x+y)
+print("sub:",x-y)
+print("multiply:",x*y)
+print("divide:",x/y)
+
+
+#silly sandwich maker
+bread=input("choose your bread white/brown:")
+vegetables=input("choose which type of vegetables you want in your sandwhich:")
+filling=input("choose your filling cheese/mayonise/cream:")
+print("here is your sandwich ready to eat")
+print(bread+" "+"bread"+" "+vegetables+" "+"vegetables"+" "+filling+" "+"filling"+" ")
+
+
+
